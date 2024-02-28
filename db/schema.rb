e0f7a924725ecce8ca492399c5245eca4561f64f
Ukaypromise <promiseukaimo@gmail.com>
2024-02-28 15:18:52 +0100

    Add account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_095935) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_28_141820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
